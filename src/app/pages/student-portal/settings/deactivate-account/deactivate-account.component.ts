d418b186782a47d6ba08290dd8ffddf6af51a523
Jason Addy <jasonaddy51@gmail.com>
2023-09-13 00:26:07 +0000

    undefined

--- src/app/pages/student-portal/settings/deactivate-account/deactivate-account.component.ts
+++ src/app/pages/student-portal/settings/deactivate-account/deactivate-account.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-deactivate-account',
   templateUrl: './deactivate-account.component.html',
-  styleUrls: ['./deactivate-account.component.scss']
+  styleUrls: ['./deactivate-account.component.scss'],
 })
 export class DeactivateAccountComponent {
-
+  deactivateAccount() {}
 }
