025ebd4addce8a2e0e0b27db5ba7e4a940be27fb
Jason Addy <jasonaddy51@gmail.com>
2023-12-27 14:28:44 +0000

    Changes

--- src/app/pages/website/home/home.component.ts
+++ src/app/pages/website/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FaqsService } from 'src/app/services/api/faqs/faqs.service';
+import { StudentsService } from 'src/app/services/api/students/students.service';
 import { GlobalsService } from 'src/app/services/core/globals';
 
 @Component({
@@ -8,9 +9,21 @@ import { GlobalsService } from 'src/app/services/core/globals';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  constructor(private globals: GlobalsService, public faqsService: FaqsService) {}
+  carouselArray: Array<any> = [];
+
+  constructor(
+    private globals: GlobalsService,
+    public faqsService: FaqsService,
+    public studentsService: StudentsService
+  ) {}
 
   async ngOnInit() {
     await this.faqsService.getFaqs();
+    await this.studentsService.getStudentProfiles();
+    console.log(this.studentsService.students);
+       this.carouselArray = [
+         ...Array(Math.ceil(this.studentsService.students.length / 3)),
+       ];
+    console.log(this.carouselArray);
   }
 }
